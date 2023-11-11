1e2a331c9126c0d1d33e17fd45dcd6db492a6ff8
munjal <munjalupadhyay@users.noreply.github.com>
2023-11-11 17:46:10 +0530

    local run

--- src/main/java/com/spark_example/App.java
+++ src/main/java/com/spark_example/App.java
@@ -1,11 +1,18 @@
 package com.spark_example;
 
+import com.spark_example.model.Employee;
+import java.util.ArrayList;
+import java.util.List;
 import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.rdd.RDD;
 import org.apache.spark.sql.Dataset;
 import org.apache.spark.sql.Row;
 import org.apache.spark.sql.SparkSession;
+import org.jetbrains.annotations.Nullable;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import scala.reflect.api.Symbols.SymbolApi;
 
 /**
  * Hello world!
@@ -26,15 +33,73 @@ public class App
     }
 
     private static void job11(SparkSession spark) {
-        Dataset<Row>  dataset= spark.read().csv("/Users/munjal-upadhyay/Downloads/word.txt");
 
-        dataset.foreachPartition(iteration -> {
-            while (iteration.hasNext()){
-                String str=iteration.next().get(0).toString();
-                logger.error(str);
-                System.out.println(str);
+        JavaRDD<String> stringJRDD = readTextFile(spark);
+        printEachElementOfRDD(stringJRDD);
+        logger.error("this is driver logs : going ahead... --> ");
+
+        JavaRDD<Employee> employeeJavaRDD = mapPartitionExample(stringJRDD);
+
+        logger.error("this is driver logs : going ahead 1 ... --> ");
+
+        printEachElementOfRDD(employeeJavaRDD);
+
+        logger.error("count {}",employeeJavaRDD.count());
+    }
+
+    @Nullable
+    private static JavaRDD<Employee> mapPartitionExample(JavaRDD<String> stringJRDD) {
+        JavaRDD<Employee> employeeJavaRDD = stringJRDD.mapPartitions(stringIterator -> {
+            List<Employee> empList = new ArrayList<>();
+            logger.error("mapPartitions initialization : this should be called only once");
+
+            if (stringIterator==null){
+                return null;
             }
+           /*
+
+           example using hasNext() and next()
+
+            while(stringIterator.hasNext()){
+                String str=stringIterator.next();
+                logger.error("creating objects for "+str);
+                String[] fields = str.split(",");
+                Long id = Long.parseLong(fields[0]);
+                String name = String.valueOf(fields[1]);
+                String surname = String.valueOf(fields[2]);
+                Employee emp = new Employee(id, name, surname);
+                logger.error("cereated object {}", emp);
+                empList.add(emp);
+            }
+
+            */
+
+            // Below is example using forEachRemaining() (better version of using hasNext() and next().)
+
+            stringIterator.forEachRemaining(str -> {
+                logger.error("creating objects for "+str);
+                String[] fields = str.split(",");
+                Long id = Long.parseLong(fields[0]);
+                String name = String.valueOf(fields[1]);
+                String surname = String.valueOf(fields[2]);
+                Employee emp = new Employee(id, name, surname);
+                logger.error("cereated object {}", emp);
+                empList.add(emp);
+            });
+            return empList.iterator(); // note : here we are returning iterator.
         });
+        return employeeJavaRDD;
+    }
+
+    private static void printEachElementOfRDD(JavaRDD<?> stringJRDD) {
+        stringJRDD.foreach(str -> logger.error("printing value of {} ",String.valueOf(str)));
+    }
+
+    private static JavaRDD<String> readTextFile(SparkSession spark) {
+        JavaRDD<String> stringJRDD = spark.sparkContext()
+            .textFile("/Users/munjal-upadhyay/Downloads/word.txt", 3)
+            .toJavaRDD();
+        return stringJRDD;
     }
 
 
---
+++ src/main/java/com/spark_example/model/Employee.java
@@ -0,0 +1,51 @@
+package com.spark_example.model;
+
+import java.io.Serializable;
+
+
+public class Employee implements Serializable {
+
+  private static final long serialVersionUID = 1504015432749744005L;
+  long id;
+  String name;
+  String surname;
+
+  public Employee(long id, String name, String surname) {
+    this.id = id;
+    this.name = name;
+    this.surname = surname;
+  }
+
+  @Override
+  public String toString() {
+    return "Employee{" +
+        "id=" + id +
+        "| name='" + name + '\'' +
+        "| surname='" + surname + '\'' +
+        '}';
+  }
+
+  public long getId() {
+    return id;
+  }
+
+  public void setId(long id) {
+    this.id = id;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public String getSurname() {
+    return surname;
+  }
+
+  public void setSurname(String surname) {
+    this.surname = surname;
+  }
+}
